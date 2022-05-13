3f76a9d34f2cbc75631895ec4069ff4dd9d976e3
aliaa gheis <aliaagheis@gmail.com>
2022-05-13 14:55:16 +0200

    fix supbytes

--- encryption/sub_bytes.v
+++ encryption/sub_bytes.v
@@ -5,10 +5,10 @@ output reg [127:0] out;
 
 wire [127:0] temp;
 
-aes_sbox w0(in[127-:32], out[127-:32]);
-aes_sbox w1(in[95-:32], out[95-:32]);
-aes_sbox w2(in[63-:32], out[63-:32]);
-aes_sbox w3(in[31-:32], out[31-:32]);
+aes_sbox w0(in[127-:32], temp[127-:32]);
+aes_sbox w1(in[95-:32], temp[95-:32]);
+aes_sbox w2(in[63-:32], temp[63-:32]);
+aes_sbox w3(in[31-:32], temp[31-:32]);
 
 always @(posedge clk) begin
     out <= temp;
